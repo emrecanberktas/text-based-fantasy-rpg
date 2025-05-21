dbb0f74d79eda280fa9fbfae1ca7f20d24066f7c
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-05-21 13:04:50 +0300

    Add: items

---
+++ src/app/data/items.js
@@ -0,0 +1,30 @@
+import {
+  GiBroadsword,
+  GiHealthPotion,
+  GiTreasureMap,
+  GiMagicSwirl,
+  GiWolfHowl,
+} from "react-icons/gi";
+
+export default {
+  kılıç: {
+    image: GiBroadsword,
+    description: "Paslı ama keskin bir kılıç, düşmanlara karşı etkili.",
+  },
+  iksir: {
+    image: GiHealthPotion,
+    description: "Sağlığı geri yükleyen büyülü bir iksir.",
+  },
+  harita: {
+    image: GiTreasureMap,
+    description: "Ejderha Tacı’na giden yolu gösteren eski bir harita.",
+  },
+  tılsım: {
+    image: GiMagicSwirl,
+    description: "Büyülü bir tılsım, tuzakları etkisizleştirir.",
+  },
+  "sadık kurt": {
+    image: GiWolfHowl,
+    description: "Sana sadık bir kurt, tehlikelerde yardımcı olur.",
+  },
+};
