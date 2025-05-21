f7d194bc6c4c92c9f38d6b1262a1be0078a25f7c
Emre Can Berktaş <emrecanberktas@gmail.com>
2025-05-21 12:55:59 +0300

    Add: Inventory and grid system

---
+++ src/app/components/Inventory.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { CiBag1 } from "react-icons/ci";
+
+interface InventoryProps {
+  items: string[];
+}
+
+function Inventory({ items }: InventoryProps) {
+  const [isInventoryOpen, setIsInventoryOpen] = useState(false);
+
+  const handleInventory = () => {
+    setIsInventoryOpen(!isInventoryOpen);
+  };
+
+  // 20 slot olacak şekilde tüm hücreleri belirle
+  const totalSlots = 20;
+  const filledSlots = items.slice(0, totalSlots);
+  const emptySlots = totalSlots - filledSlots.length;
+
+  return (
+    <div className="p-4">
+      <h2 className="mb-4 text-lg font-bold">Inventory</h2>
+
+      {!isInventoryOpen && (
+        <button
+          onClick={handleInventory}
+          className="text-3xl text-white hover:text-yellow-300 transition"
+        >
+          <CiBag1 />
+        </button>
+      )}
+
+      {isInventoryOpen && (
+        <div className="grid grid-cols-4 grid-rows-5 gap-2">
+          {filledSlots.map((item, index) => (
+            <div
+              key={`item-${index}`}
+              className="w-16 h-16 bg-gray-700 border border-amber-950 flex items-center justify-center text-white rounded"
+            >
+              {item}
+            </div>
+          ))}
+
+          {Array.from({ length: emptySlots }).map((_, index) => (
+            <div
+              key={`empty-${index}`}
+              className="w-16 h-16 bg-gray-700 opacity-50 border border-yellow-900 rounded"
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Inventory;
